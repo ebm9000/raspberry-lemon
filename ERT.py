4386c27070d6aca1c837e5913d9ba2b6b3033dc4
Kyle Lewis <kylelewis57@hotmail.com>
2019-12-25 12:51:58 -0500

    Add files via upload
    The math behind the ERT program is done (formatting could be employed to make it look better. Began coding a program selection menu, but a problem is preventing me from implementing an exit function;

--- ERT.py
+++ ERT.py
@@ -1,35 +1,83 @@
-#######
-#Estimated Time (in hours) Calculator
-#######
+#Programs:
+#1: Calculate Estimated Runtime (in hours)
+    #Author: EBM9000
+    #Associated function: timeimp()
+#2: Create app_config.xml file (?)
 
-#Variables to be run through function "timebound", where:
-    #VAR[0] is the number of Days, Hours, Minutes or Seconds
-    #VAR[1] are their names, so the errors can output properly
-Days = [0, "Days"]
-Hours = [0, "Hours"]
-Mins = [0, "Minutes"]
-Secs = [0, "Seconds"]
+def programslist():
+    #Includes formatting with ~ and  * characters
+    programs = [
+        ["1", "Calculate Estimated Runtime (in hours)", "EBM9000", "timeimp()"],
+        ["2", "Create a config file for a project", "EBM9000", "createconfig()"],
+        ["~"*3,"~"*40,"~"*15,"~"*5],
+        ["H", "Help Document 1", "KurzedMetal", "N/A"],
+        ["E", "Exit the program", "N/A", "exit"],
+        ["*"*3,"*"*40,"*"*15,"*"*5]
+    ]
+    #Prints the header and the first set of *'s above the table (can't be in the loop)
+    print("*"*3,"*"*40,"*"*15,"*"*5)
+    print("{: <3} {: <40} {: <15} {: <5}".format("#", "Program Name", "Author", "Function Call"))
+    
+    #prints each program in the format above
+    for row in programs:
+        print("{: <3} {: <40} {: <15} {: <5}".format(*row))
+
+    #Verifies that the program selection variable is reset on each return
+    progSelect = 0
+    progSelect = input("\nSelect a program: ")
+
+    #Loops to determine what to execute based on the progSelect variable
+    if progSelect == str(1):
+        print(progSelect)
+        timeinp()
+    
+    elif progSelect == "H" or "h":
+        print(progSelect)
+        print("\nI used this link to help format my columns in the program list: \nhttps://stackoverflow.com/questions/9989334/create-nice-column-output-in-python\nThanks to KurzedMetal for the post!\n")
+        programslist()
+
+    #does not currently funciton; always executes the *above* statement if a letter is entered
+    elif "E" or "e" or "exit" or "Exit" or "EXIT" in progSelect:
+        print(progSelect)
+        print("Goodbye!")
+
+    else:
+        print(progSelect)
+        print("Error: selection not valid. Try again\n")
+        programslist()
+    
 
 def timeinp():
+    print("\n##############\nCalculate Estimated Runtime (in hours)\nby EBM9000\n##############\n")
+    
+    #Variables to be run through function "timebound", where:
+    #VAR[0] is the number of Days [Will add in later], Hours, Minutes or Seconds
+    #VAR[1] are their names, so the errors can output properly
+
+    Days = [0, "Days"]
+    Hours = [0, "Hours"]
+    Mins = [0, "Minutes"]
+    Secs = [0, "Seconds"]
 
     def timebound(x , y):
+                    
         if "Days" in y:
             while True:
                 try:
                     x = Days[0] = int(input( str(y) + " Elapsed: "))
                 
-                #If a float, string etc is entered, print an error message and rerun the script
                 except ValueError:
                     print("Error: " + str(y) + " value must be an integer")
                     timebound(x , y)
-                
-                #Explained in the elif statements (better explained w/ logic)
+            
+                #If a negative or a value over 24 is entered, run the appropriate if statement
+
                 if x < 0:
                     print("Error: " + str(y) + " value must be positive")
                     timebound(x , y)
-
-                break
                 
+                break
+
         elif "Hours" in y:
             while True:
                 try:
@@ -38,7 +86,7 @@ def timeinp():
                 except ValueError:
                     print("Error: " + str(y) + " value must be an integer")
                     timebound(x , y)
-               
+            
                 #If a negative or a value over 24 is entered, run the appropriate if statement
 
                 if x < 0:
@@ -46,8 +94,7 @@ def timeinp():
                     timebound(x , y)
                 
                 elif x >= 24:
-                    print("Error: " + str(y) +  " value cannot be greater than 24")
-                    timebound(x , y) 
+                    print("Error: " + str(y) + " value must be less than 24")
                 break
         
         elif "Minutes" in y:
@@ -89,32 +136,29 @@ def timeinp():
                 elif x >= 60:
                     print("Error: " + str(y) +  " value cannot be greater than 59")
                     timebound(x , y)
-            
+                break
 
+    #Incorrect results here!:       
+    def statistics():
+        #Defining stats variables
+        percentage= 0.01 * float(input("Enter the Task Progress as a percent (enter as is and omit the o/o sign): "))
+        totalinHours = float((24*Days[0] + Hours[0]) + (Mins[0] / 60) + (Secs[0] / 3600))  
+
+        #Executing calculations
+        estimatedRuntime = totalinHours / percentage
+        estimatedRemaining = estimatedRuntime - totalinHours
+
+        #Printing Results
+        print("\nEstimated Remaining Runtime in hours: " + str(round(estimatedRemaining, 4)))
+        print("Estimated Total Runtime in hours: " + str(round(estimatedRuntime, 4)))
+        
     timebound(Days[0] , Days[1])
-    print(Days[0])
-   
     timebound(Hours[0] , Hours[1])
-    print(Hours[0])
-    
     timebound(Mins[0] , Mins[1])
-    print(Mins[0])
-    
     timebound(Secs[0] , Secs[1])
-    print(Secs[0])
-timeinp()
- 
+    statistics()
+    input("Execution complete. Press Enter to exit...")
 
-
-#   elapsedTotal = float(elapsedHours + (elapsedMins / 60) + (elapsedSecs / 3600))
-#    percentage= float(input("Enter the Task Progress as a percent: (i.e: type 23.401% as '23.401'; 0.159% as '0.159', etc) ")) / 100
-
-#   estimatedRuntime = elapsedTotal / percentage
-#    estimatedRemaining = estimatedRuntime - elapsedTotal
-#    print("\n")
-
-#    print("Estimated Total Runtime in hours: " + str(round(estimatedRuntime, 4)))
-#    print("Estimated Remaining Runtime in hours: " + str(round(estimatedRemaining, 4)))
-
-
+print("##############\nBoinc Pack\nby EBM9000\n##############\n")
+programslist()
 
