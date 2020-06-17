f6c5ec02f06dbe192d12a8ce7c0f3198233df437
Kyle Lewis <kylelewis57@hotmail.com>
2020-06-17 14:00:44 -0400

    Add files via upload
    changed variable names to be more logical;

--- BlackJack.py
+++ BlackJack.py
@@ -1,10 +1,14 @@
 from random import choice as rand
 from os import system as system
 
+#round number
 stage = [1]
+
+#total score
 score = [0]
 
 def script():
+    #possible values you can roll; resets each round to ensure proper RNG
     rngroll = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52]
     deck = [
         [1, "Ace of Clubs", "Black", 11, 1],
@@ -60,29 +64,30 @@ def script():
         [51, "King of Diamonds", "Red", 10],
         [52, "King of Hearts", "Red", 10]
         ]
-    hand = [int(0)]
+    hand = [0]
     aceCount = [0]
     
     def drawcard():
-        #chooses a random number 1-52
+        #chooses a random number 1-52; based off of the rngroll table
         x = rand(rngroll)        
-        #removes card after drawn
+        #removes card after drawn; based off of value drawn, not the position in table
         rngroll.remove(x)
 
-        #adds the name of the card to the player's hand[1:]
+        #adds the name of the card to the player's hand[1:]; 'x' was a value, but now it is used to get a value based on >position< in the >deck< table; offset by 1
         hand.append(deck[x-1][1])
         
-        #Adds Value to the player's hand[0]
+        #Adds Value to the player's hand[0]; aces are assumed as 11 for now
         hand[0] += int(deck[x-1][3])
 
-        #Sets up a check to see if you hold an ace after you go over 21
+        #If you go over 21, checks to see if you have an ace
         if 'Ace' in deck[x-1][1]:
             aceCount[0] += 1
         
-        menu()
+        scorecheck()
 
-    def startGameCheck():
-        #if hand[0], your value, is 0 then there is no game in progress.
+    #basic UI
+    def menu():
+        #Ends the game if 5 rounds are completed
         if stage[0] == 6:
             system('cls')
             print("You finished all 5 rounds. Your score was " + str(score[0]))
@@ -90,16 +95,22 @@ def script():
             exit()
 
         else:
-            if int(hand[0]) == 0:
-                system('cls')
-                print("\nRound " + str(stage[0]) + " of 5\n")
+            #if stage[0] is 1, start a game (default when launched)
+            system('cls')
+            print("\nRound " + str(stage[0]) + " of 5\n")
+            print("Your Score: " + str(score[0]))
+
+            #stage[0] is never 0; but if it is set to == 1 here, then the script does not print your hand!
+            if int(stage[0]) == 0:
                 print("Your Hand: Empty!")
-                print("Your Score: " + str(score[0]))
             else: 
                 print("\nYour Hand: " + str(hand[1:]))
-            print("Deck Value: " + str(hand[0]) + "\n")
+           
+            #makes more logical sense to print Hand Value after printing the contents of your Hand
+            print("Hand Value: " + str(hand[0]) + "\n")
 
-    def scoring():
+    #Tallys game score, reruns script() to reset rngroll, deck and hand. var 'score' is exclusive
+    def eor_scoring():
         if hand[0] < 21:
             score[0] += 3*(21-hand[0])
         elif hand[0] == 21:
@@ -109,29 +120,33 @@ def script():
         stage[0] += 1
         script()
 
-    def menu():
-        #prints appropriate text based on if a game is ongoing
-        startGameCheck()
+    #runs menu() followed by itself; checks current hand value and catches if over 20
+    def scorecheck():
+
+        menu()
         
         if int(hand[0]) == 21: 
             print("You Win!")
             input("Press Enter to go to the next round... ")
-            scoring()
+            eor_scoring()
         
-        #If you're over 21...
-        elif hand[0] not in range(0,21):
+        #If your scoe is over 21... can't become negative at any time
+        elif hand[0] > 21:
             #Ace check; if you don't have one the game is over
             if aceCount[0] == 0:
                 print("You got over 21!")
                 input("Press Enter to go to the next round...")
-                scoring()
+                eor_scoring()
 
             #Ace check; if you have an ace your hand value drops by 10 and the game resumes
             elif aceCount[0] > 0:
                 print("You got over 21, but you drew 1 or more Aces! One of them has been devalued to 1 to let you keep drawing")
+                
+                #devalues a single ace by 10, removes it from the deck, then continues
                 hand[0] += -10
                 aceCount[0] += -1
-                menu()
+                scorecheck()
+    
         
         else:
             while True:
@@ -143,8 +158,8 @@ def script():
                 if card == int(1):
                     drawcard()
                 elif card == int(2):
-                    scoring()
+                    eor_scoring()
     
     #executes Menu when script() is ran
-    menu()
+    scorecheck()
 script()
